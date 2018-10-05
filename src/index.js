ce5b7d4398e556d9a0c6d97f4da30e120957019e
nhithai <nhithai88@gmail.com>
2018-10-05 15:26:10 +0700

    React-router problem with gh-pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+    
+ReactDOM.render(
+    <Router history={hashHistory}>
+      <App />
+    </Router>,
+  document.getElementById('root'));
+
 registerServiceWorker();
